aec7244e30a300f4643c0ab8efa9783c914ec076
Nathan Lawrence <lawrencen@missouri.edu>
2016-03-03 17:21:10 -0600

    Starting column app logic.

--- js/app.js
+++ js/app.js
@@ -6,6 +6,13 @@
 	prettify.controller('TableController', function TableController($scope, $http){
 		$http.get('tables.json').success(function(data){
 			$scope.tables = data;
+			$scope.tblOrderField = "record_count";
+		});
+	});
+
+	prettify.controller('ColumnController', function ColumnController($scope, $http){
+		$http.get('columns.json').success(function(data){
+			$scope.columns = data;
 		});
 	});
 
