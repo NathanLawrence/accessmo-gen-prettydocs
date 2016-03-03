e6a119e7dc2fea8682be646766127682a819c149
Nathan Lawrence <lawrencen@missouri.edu>
2016-03-03 17:10:49 -0600

    Table inventory working!

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 (function(){
-	
+	console.log('activated');
+
+	var prettify = angular.module('prettify', []);
+
+	prettify.controller('TableController', function TableController($scope, $http){
+		$http.get('tables.json').success(function(data){
+			$scope.tables = data;
+		});
+	});
+
 })();
